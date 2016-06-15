ce6895a7ef137978103d4e7f835f1d184cd08e9b
eric <erichub>
2016-06-15 12:15:16 +0200

    Amélioration FormLogin

--- FormLogin.php
+++ FormLogin.php
@@ -1,77 +1,61 @@
 <?php
-
-
 /**
  * class FormLogin
  *
  */
 class FormLogin
 {
-
   /** Aggregations: */
-
   /** Compositions: */
-
    /*** Attributes: ***/
-
   /**
-
    *
    * @access public
    */
-  <input type ="email", name="email", placeholder="Votre e-mail"/>;
-
   public $email = [
-    'field' => 'input';
-    'type' => 'email';
-    'name' => 'email';
-    'Attributes' => [
-      'placeholder' => 'Votre e-mail';
-    ];
-
+    'field' => 'input',
+    'type' => 'email',
+    'name' => 'email',
+    'attributes' => [
+      'placeholder' => 'Votre e-mail',
+    ],
   ];
-
   /**
    *
    * @access public
    */
-  <input type ="password", name="password", placeholder="Votre mot de passe"/>
-
   public $pass = [
-    'field' => 'input';
-    'type' => 'password';
-    'name' => 'password';
-    'Attributes' => [
-      'placeholder' => 'Votre mot de passe';
-    ];
-
+    'field' => 'input',
+    'type' => 'password',
+    'name' => 'pass',
+    'attributes' => [
+      'placeholder' => 'Votre pass',
+    ],
   ];
-
   public function render() {
-      $form_markup = '<form method='post'>';
-      $form_markup .= $this->generateField($this->email);
-      $form_markup .= $this->generateField($this->pass);
-      $form_markup .= '</form>';
-
-      return $form_markup;
+    $form_markup = '<form method="post">';
+    $form_markup .= $this->generateField($this->email);
+    $form_markup .= $this->generateField($this->pass);
+    $form_markup .= '</form>';
+    return $form_markup;
   }
-
-
-
-
   private function generateField($field) {
-  $field_markup .= '<' . $field['field']
-  . ' type="' . $this->email['type']
-  . '" name="' . $this->email['name'] . '" />';
-
-  return $field_markup;
+    $field_markup = NULL;
+    $tag_open = NULL;
+    $attributes = NULL;
+    if ($field['field'] == 'input') {
+      $tag_open = '<' . $field['field'] . ' type="' . $field['type'] . '" ';
+      $tag_close = ' />';
+    }
+    elseif ($field['field'] == 'textarea') {
+      $tag_open = '<' . $field['field'] . '>';
+      $tag_close = '</' . $field['field'] . '>';
+    }
+    foreach ($field['attributes'] as $attribute => $value) {
+      $attributes .= $attribute . '="' . $value . '" ';
+    }
+    $field_markup = $tag_open . ' name="' . $field['name'] . '" ' . $attributes . $tag_close;
+    return $field_markup;
   }
-
-  ?>
-
-
-
-
-
 } // end of FormLogin
 ?>
