b41e48c17affcf939373c420713564f7fe35b7d1
eric <erichub>
2016-06-15 12:07:21 +0200

    Correction FormLogin

--- FormLogin.php
+++ FormLogin.php
@@ -1,77 +1,50 @@
 <?php
-
-
 /**
  * class FormLogin
  *
  */
 class FormLogin
 {
-
   /** Aggregations: */
-
   /** Compositions: */
-
    /*** Attributes: ***/
-
   /**
-
    *
    * @access public
    */
-  <input type ="email", name="email", placeholder="Votre e-mail"/>;
-
   public $email = [
-    'field' => 'input';
-    'type' => 'email';
-    'name' => 'email';
-    'Attributes' => [
-      'placeholder' => 'Votre e-mail';
-    ];
-
+    'field' => 'input',
+    'type' => 'email',
+    'name' => 'email',
+    'attributes' => [
+      'placeholder' => 'Votre e-mail',
+    ],
   ];
-
   /**
    *
    * @access public
    */
-  <input type ="password", name="password", placeholder="Votre mot de passe"/>
-
   public $pass = [
-    'field' => 'input';
-    'type' => 'password';
-    'name' => 'password';
-    'Attributes' => [
-      'placeholder' => 'Votre mot de passe';
-    ];
-
+    'field' => 'input',
+    'type' => 'password',
+    'name' => 'pass',
+    'attributes' => [
+      'placeholder' => 'Votre pass',
+    ],
   ];
-
   public function render() {
-      $form_markup = '<form method='post'>';
-      $form_markup .= $this->generateField($this->email);
-      $form_markup .= $this->generateField($this->pass);
-      $form_markup .= '</form>';
-
-      return $form_markup;
+    $form_markup = '<form method="post">';
+    $form_markup .= $this->generateField($this->email);
+    $form_markup .= $this->generateField($this->pass);
+    $form_markup .= '</form>';
+    return $form_markup;
   }
-
-
-
-
   private function generateField($field) {
-  $field_markup .= '<' . $field['field']
-  . ' type="' . $this->email['type']
-  . '" name="' . $this->email['name'] . '" />';
-
-  return $field_markup;
+    $field_markup = '<' . $field['field']
+     . ' type="' . $field['type']
+     . '" name="' . $field['name'] . '" />';
+    return $field_markup;
   }
-
-  ?>
-
-
-
-
 
 } // end of FormLogin
 ?>
