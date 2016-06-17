3c424b5e8b3b0e8440a4bc16b2b93b3e8d80ba14
eric <erichub>
2016-06-17 16:59:17 +0200

    Ajout Autoloader

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
+
 <?php
+/*
   require_once 'base/ContentEntityInterface.php';
   require_once 'base/Entity.php';
   require_once 'base/UserManager.php';
@@ -21,6 +23,63 @@ print '<code><em>From class ' . __CLASS__ . ' in function ' . __FUNCTION__ . '()
 
 
 $user = UserEntity::create($_POST)->save();
+
+
+?>
+
+
+<?php
+  /*
+  function __autoload ($class_name) {
+
+
+  print '<pre>$var => <br />';
+  print_r($class_name);
+  print "</pre>";
+  print '<code><em>From class ' . __CLASS__ . ' in function ' . __FUNCTION__ . '() line ' . __LINE__ . ' in file ' . __FILE__ . '</em>.</code>';
+
+  require_once 'base/' . $class_name . '.php';
+  die();
+
+  }
+
+  require_once 'base/ContentEntityInterface.php';
+  require_once 'base/EntityManager.php';
+  require_once 'base/UserManager.php';
+  require_once 'base/UserEntity.php';
+  require_once 'base/UserEntityCompany.php';
+  */
+  require_once 'base/Entity.php';
+
+
+  function autoload ($class) {
+/*
+    print '<pre>$var => <br />';
+    print_r($class);
+    print "</pre>";
+    print '<code><em>From class ' . __CLASS__ . ' in function ' . __FUNCTION__ . '() line ' . __LINE__ . ' in file ' . __FILE__ . '</em>.</code>';
+*/
+      }
+
+
+  $_POST = [
+    'name' => 'JP',
+    'email' => 'jp@aston.fr',
+    'pass' => 'p@$$w0rd',
+    'localisation' => 'Paris',
+    'age' => '25',
+  ];
+
+/*
+print '<pre>$user => <br />';
+print_r($user);
+print "</pre>";
+print '<code><em>From class ' . __CLASS__ . ' in function ' . __FUNCTION__ . '() line ' . __LINE__ . ' in file ' . __FILE__ . '</em>.</code>';
+*/
+
+$user = new UserEntity();
+
+//$user = UserEntity::create($_POST)->save();
 
 
 ?>
